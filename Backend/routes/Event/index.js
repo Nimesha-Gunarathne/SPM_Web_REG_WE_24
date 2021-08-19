c5df341dd46d2f5315b50f38d45c1d4a7ae3f1ca
Nimesha98 <nimesha.g98@gmail.com>
2021-08-19 23:40:19 +0530

    Routes backend admin

---
+++ Backend/routes/Event/index.js
@@ -0,0 +1,14 @@
+const routes = require("express").Router();
+const EventsRoutes = require("../../controllers/Event.controller");
+
+routes.post("/create-event", EventsRoutes.createEvents);
+routes.get("/getAllEvents", EventsRoutes.getAllEvents);
+routes.get("/getEventByid/:id", EventsRoutes.getEventDetailsByEventID);
+
+
+routes.put("/update-event/:id",EventsRoutes.UpdateEventDetailsByEventID);
+  
+
+
+
+module.exports = routes;
